366f091efbe13118699468eeb135a6b5ebe89a20
EzhNEEsh <oyaiskander@yandex.ru>
2024-08-01 11:34:04 +0300

    test: test for breaks exist added

--- test/test_pcmreader.cpp
+++ test/test_pcmreader.cpp
@@ -21,3 +21,20 @@ TEST(PCMReader_Group, BreaksDontExist_Test) {
         ASSERT_EQ(0, countOfBreaks);
     }
 }
+
+TEST(PCMReader_Group, BreaksExist_Test) {
+    std::string directoryPath = "data_raw_32_rand_ch_offs_break";
+    PCMReader pcmReader;
+
+    std::filesystem::directory_iterator dirIterator;
+    dirIterator = std::filesystem::directory_iterator{directoryPath};
+
+    for (auto &file : dirIterator) {
+        std::string filePath = file.path().string();
+        std::vector<std::string> breaksBuf;
+        int countOfBreaks;
+        std::tie(countOfBreaks, breaksBuf) = pcmReader.getCountOfBreaks(filePath);
+
+        ASSERT_EQ(128, countOfBreaks);
+    }
+}
