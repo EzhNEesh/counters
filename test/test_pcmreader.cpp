42ab28f7c17464e67e8dad8005d130220582b281
EzhNEEsh <oyaiskander@yandex.ru>
2024-08-01 11:32:33 +0300

    test: test for no breaks added

---
+++ test/test_pcmreader.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include <vector>
+#include <filesystem>
+
+#include "../src/pcmreader.h"
+#include "../src/pcmreader.cpp"
+
+TEST(PCMReader_Group, BreaksDontExist_Test) {
+    std::string directoryPath = "data_raw_32_rand_ch_offs";
+    PCMReader pcmReader;
+
+    std::filesystem::directory_iterator dirIterator;
+    dirIterator = std::filesystem::directory_iterator{directoryPath};
+
+    for (auto &file : dirIterator) {
+        std::string filePath = file.path().string();
+        std::vector<std::string> breaksBuf;
+        int countOfBreaks;
+        std::tie(countOfBreaks, breaksBuf) = pcmReader.getCountOfBreaks(filePath);
+
+        ASSERT_EQ(0, countOfBreaks);
+    }
+}
